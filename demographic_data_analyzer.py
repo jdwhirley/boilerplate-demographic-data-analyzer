4ffb79839288fd641af681c0f524dd1828ada664
J.D. Whirley <86266189+jdwhirley@users.noreply.github.com>
2021-07-02 13:48:37 -0700

    Update demographic_data_analyzer.py

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -3,10 +3,11 @@ import pandas as pd
 
 def calculate_demographic_data(print_data=True):
     # Read data from file
-    df = None
+    df = pd.read_csv('adult.data.csv')
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = None
+    race_count = df.set_index(["race"])
+    race_count.sort_index(inplace = True)
 
     # What is the average age of men?
     average_age_men = None
